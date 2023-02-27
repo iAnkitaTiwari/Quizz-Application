55962f3df07ebabcf063dfc4d6cfec6d1ae049df
Ankia Tiwati <ankitajtiwari18@gmail.com>
2023-02-27 11:09:14 +0100

    Initial commit

---
+++ quiz/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Complexity,Result,Question,Course
+admin.site.register(Complexity)
+admin.site.register(Result)
+admin.site.register(Question)
+admin.site.register(Course)
